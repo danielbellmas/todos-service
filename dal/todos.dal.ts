c808f8e0505288c95639f975c72fb1c782f5dedb
Daniel Bellmas <76179660+danielbellmas@users.noreply.github.com>
2023-07-12 19:07:37 +0300

    Fix dal's function return types

--- dal/todos.dal.ts
+++ dal/todos.dal.ts
@@ -2,19 +2,19 @@ import type { Todo } from '../models/todo';
 import { TodoMongoModel } from '../models/todo';
 
 export class TodoDal {
-  async getAll() {
+  async getAll(): Promise<Todo[]> {
     return TodoMongoModel.find();
   }
 
-  async create(todo: Omit<Todo, '_id'>) {
+  async create(todo: Omit<Todo, '_id'>): Promise<Todo> {
     return TodoMongoModel.create(todo);
   }
 
-  async updateById({ _id, title, deadline }: Todo) {
+  async updateById({ _id, title, deadline }: Todo): Promise<Todo | null> {
     return TodoMongoModel.findByIdAndUpdate(_id, { title, deadline }, { new: true });
   }
 
-  async delete(id: string) {
+  async delete(id: string): Promise<Todo | null> {
     return TodoMongoModel.findByIdAndDelete(id);
   }
 }
