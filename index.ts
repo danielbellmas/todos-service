ba81cece280256533a385fddac3fa5b9a560d292
Daniel Bellmas <76179660+danielbellmas@users.noreply.github.com>
2023-07-12 15:55:40 +0300

    Use routes with the express app

--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
 import express from 'express';
 import type { ConnectOptions } from 'mongoose';
 import mongoose from 'mongoose';
+import todosRoutes from './routes/todos.routes';
 import config from './config';
 
-const { database } = config;
+const app = express();
+
+app.use(express.json());
+
+app.use('/todos', todosRoutes);
+
+const { port, database } = config;
 
 (async () => {
   try {
     await mongoose.connect(database.url, database.options as ConnectOptions);
     console.log('Connected to MongoDB');
+
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port} 🚀`);
+    });
   } catch (error) {
     console.error('Failed to connect to MongoDB', error);
   }
